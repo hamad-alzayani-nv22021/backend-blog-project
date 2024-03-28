ecc77ab2b432519cdb0bd22746391e0046a8e735
hamad-alzayani-nv22021 <154792345+hamad-alzayani-nv22021@users.noreply.github.com>
2024-03-28 11:54:19 +0000

    12 - Starting the Back-end

--- server.js
+++ server.js
@@ -7,5 +7,8 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
 res.send("server is running...");
-
 });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
