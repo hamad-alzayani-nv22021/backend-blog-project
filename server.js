cfe0d1dfb754c6eb1dabb3ff91225d4de58ca194
hamad-alzayani-nv22021 <154792345+hamad-alzayani-nv22021@users.noreply.github.com>
2024-03-31 06:30:26 +0000

    Video14 15:18

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-import express from "express"
-import dotenv  from 'dotenv'
+import express from "express";
+import dotenv from "dotenv";
 import connectDB from "./config/db";
+
+// Routes
+import userRoutes from "./routes/userRoutes";
 
 dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-res.send("server is running...");
+app.get("/", (req, res) => {
+  res.send("Server is running...");
 });
+
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
